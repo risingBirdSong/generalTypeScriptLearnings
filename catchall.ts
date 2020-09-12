6075aa44e585a4b039c9f8547e84681b6d88fcf7
risingbirdsong <heregoingthere@outlook.com>
2020-09-11 20:37:42 -0700

    cool way to get the nested key types I saw on discord

---
+++ catchall.ts
@@ -0,0 +1,24 @@
+
+// question How can I get the key of the nested objects pls ?
+// he wants the nested keys but is getting the outer keys
+//credit Kirdes
+const RATIOS = {
+  WELCOME: {
+    '--welcome-100': 3,
+    '--welcome-200': 4.5,
+  },
+  HOME: {
+    '--home-100': 3,
+    '--home-200': 4.5,
+  },
+};
+
+type RatiosName = keyof typeof RATIOS
+// "WELCOME" "HOME"
+
+type KeyOfDistributed<T> = T extends any ? keyof T : never;
+type ZZ = KeyOfDistributed<typeof RATIOS[keyof typeof RATIOS]>
+//credit GerritO
+
+type KeyOfDistributedRemake<T> = T extends object ? keyof T : never;
+type innerKeys = KeyOfDistributedRemake<typeof RATIOS[keyof typeof RATIOS]>

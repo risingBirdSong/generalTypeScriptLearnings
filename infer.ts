0aa6f9cb3e30d1dbfc4a28ee85006876ce0df22f
risingbirdsong <heregoingthere@outlook.com>
2020-09-13 10:39:56 -0700

    thats how it works

--- infer.ts
+++ infer.ts
@@ -1,8 +1,15 @@
 
 type FlattenIfArray<T> = T extends (infer R)[] ? R : T
 
+
+//https://dev.to/miracleblue/how-2-typescript-serious-business-with-typescripts-infer-keyword-40i5
+
 type GetReturnType<original extends Function> =
   original extends (...x: any[]) => infer returnType ? returnType : never
+
+type remakeGetReturnType<original extends Function> = original extends (...args: any[]) => infer whatType ? whatType : never;
+
+type doItAgain<func extends Function> = func extends (...args: any[]) => infer returnedType ? returnedType : never;
 
 function add(num: number) {
   return num * 2;
@@ -26,3 +33,9 @@ function eitherTorTArr<T>(input: T, arr: boolean) {
 
 //hmm why is this unknown, I was expecting it to be the same inference that eitherTorTArr has. T | T[] 
 type inferEitherOfTArr = GetReturnType<typeof eitherTorTArr>
+
+// ah I think this answers it, it's how it works... from the docs
+//https://www.typescriptlang.org/docs/handbook/utility-types.html#returntypet
+//ReturnType<Type>
+type T2 = ReturnType<<T>() => T>;
+//    ^ = type T2 = unknown

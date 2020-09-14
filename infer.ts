42e3a1082041750bad504df879d0f669f33354fe
risingbirdsong <heregoingthere@outlook.com>
2020-09-13 18:18:51 -0700

    further examples and how to add and subtract modifieers

--- infer.ts
+++ infer.ts
@@ -275,3 +275,41 @@ type myT2 = remakeUnpacked<() => {tada : string}>;
 type myT3 = remakeUnpacked<Promise<string>>;
 type myT4 = remakeUnpacked<Promise<string[]>>;
 type myT5 =remakeUnpacked<remakeUnpacked<remakeUnpacked<string>>>;
+
+type FunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? K : never;
+}[keyof T];
+type FunctionProperties<T> = Pick<T, FunctionPropertyNames<T>>;
+
+type NonFunctionPropertyNames<T> = {
+  [K in keyof T]: T[K] extends Function ? never : K;
+}[keyof T];
+type NonFunctionProperties<T> = Pick<T, NonFunctionPropertyNames<T>>;
+
+interface Part {
+  id: number;
+  name: string;
+  subparts: Part[];
+  updatePart(newName: string): void;
+}
+
+type T40 = FunctionPropertyNames<Part>; // "updatePart"
+type T41 = NonFunctionPropertyNames<Part>; // "id" | "name" | "subparts"
+type T42 = FunctionProperties<Part>; // { updatePart(newName: string): void }
+type T43 = NonFunctionProperties<Part>; // { id: number, name: string, subparts: Part[] }
+
+
+interface readonlyOptional {
+  readonly num? : number;
+  readonly str? : string;
+}
+interface mutableRequired {
+  num : number;
+  str : string;
+}
+
+type MutableRequired<T> = { -readonly [P in keyof T]-?: T[P] }; // Remove readonly and ?
+type ReadonlyPartial<T> = { +readonly [P in keyof T]+?: T[P] }; // Add readonly and ?
+
+type convertA = MutableRequired<readonlyOptional>;
+type convertB = ReadonlyPartial<mutableRequired>;
